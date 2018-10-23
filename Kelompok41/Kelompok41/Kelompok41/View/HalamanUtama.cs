8c0a606a255099dead369123ca48a9cd3c6e19b7
ryolu22 <44153960+ryolu22@users.noreply.github.com>
2018-10-23 17:48:42 +0700

    TugasModul1
    Kelompok41

---
+++ Kelompok41/Kelompok41/Kelompok41/View/HalamanUtama.cs
@@ -0,0 +1,60 @@
+﻿using Kelompok41.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Kelompok41
+{
+    public class HalamanUtama : ContentPage
+    {
+        public HalamanUtama()
+        {
+            this.Title = "Data Mahasiswa";
+
+            StackLayout stacklayout = new StackLayout();
+            Button button = new Button();
+            button.Text = "Tambah Data";
+            button.Clicked += Button_Tambah_Clicked;
+            stacklayout.Children.Add(button);
+
+            button = new Button();
+            button.Text = "Lihat Data";
+            button.Clicked += Button_Lihat_Clicked;
+            stacklayout.Children.Add(button);
+
+            button = new Button();
+            button.Text = "Edit";
+            button.Clicked += Button_Edit_Clicked;
+            stacklayout.Children.Add(button);
+
+            button = new Button();
+            button.Text = "Hapus";
+            button.Clicked += Button_Hapus_Clicked;
+            stacklayout.Children.Add(button);
+
+            Content = stacklayout;
+        }
+
+        private async void Button_Tambah_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HalamanTambahData());
+        }
+        private async void Button_Lihat_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HalamanLihatData());
+        }
+
+        private async void Button_Edit_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HalamanEditData());
+        }
+        private async void Button_Hapus_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new HalamanHapusData());
+        }
+
+    }
+}
